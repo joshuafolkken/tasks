9219fd9c179596e1d1fed5f7d711c2c28afbb750
joshuafolkken <joshuafolkken@gmail.com>
2026-02-05 11:42:09 +0700

    supabase auth #19 base code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,28 @@
 	import { page } from '$app/state'
 	import { locales, localizeHref } from '$lib/paraglide/runtime'
 	import './layout.css'
+	import { invalidate } from '$app/navigation'
 	import favicon from '$lib/assets/favicon.svg'
+	import { onMount } from 'svelte'
 
-	let { children } = $props()
+	let { data, children } = $props()
+	let { supabase, session } = $derived(data)
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((_event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth')
+			}
+		})
+
+		return () => data.subscription.unsubscribe()
+	})
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<title>User Management</title>
+	<link rel="icon" href={favicon} />
+</svelte:head>
 
 {@render children()}
 <div style="display:none">
