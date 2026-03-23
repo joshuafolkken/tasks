25915d081cae44119f8c42194e02bf81066186f4
joshuafolkken <joshuafolkken@gmail.com>
2026-03-23 16:08:43 +0700

    update auth #96

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,4 +1,4 @@
-import type { Session, User } from 'better-auth/minimal'
+import type { Session, User } from 'better-auth'
 
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
