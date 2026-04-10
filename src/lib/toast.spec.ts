07597f9e870b895152b35407b831289cb37d0e17
joshuafolkken <joshuafolkken@gmail.com>
2026-04-10 13:28:34 +0700

    Fix toast spec possibly-undefined CI type error #145

--- src/lib/toast.spec.ts
+++ src/lib/toast.spec.ts
@@ -27,12 +27,12 @@ describe('toast store', () => {
 		toast.push_error('Error A')
 		toast.push_error('Error B')
 
-		const [first_item] = toast_store.items
+		const first_id = toast_store.items[0]?.id ?? -1
 
-		toast.dismiss(first_item.id)
+		toast.dismiss(first_id)
 
 		expect(toast_store.items).toHaveLength(1)
-		expect(toast_store.items[0].message).toBe('Error B')
+		expect(toast_store.items[0]?.message).toBe('Error B')
 	})
 
 	it('auto-dismisses after 4 seconds', () => {
